6e67a6ea5ef6dd1454a0ba3901839615c8d26c63
srikanth3333 <srikanth939196@gmail.com>
2022-11-23 10:06:07 +0530

    homepage modified

---
+++ components/HomeCard.js
@@ -0,0 +1,77 @@
+import { Input,Select, DatePicker,Tabs } from 'antd';
+import moment from 'moment';
+import React from 'react';
+
+function HomeCard({title,countArray}) {
+  return (
+    <div>
+        <div className="card mt-3">
+        <div className="card-body">
+            <h2 className="title">{title}</h2>
+            <div className="row">
+                    <div className="col-lg-6">
+                        <label htmlFor="">Circle</label>
+                        <Select
+                            allowClear
+                            showSearch
+                            style={{ width: '100%' }}
+                            >
+                            <Select.Option> 
+                                {"val"}
+                            </Select.Option>
+                        </Select>
+                    </div>
+                    <div className="col-lg-6">
+                        <label htmlFor="">Division</label>
+                        <Select
+                            allowClear
+                            showSearch
+                            style={{ width: '100%' }}
+                            >
+                            <Select.Option> 
+                                {"val"}
+                            </Select.Option>
+                        </Select>
+                    </div>
+                    <div className="col-lg-6">
+                        <label htmlFor="">Zone</label>
+                        <Select
+                            allowClear
+                            showSearch
+                            style={{ width: '100%' }}
+                            >
+                            <Select.Option> 
+                                {"val"}
+                            </Select.Option>
+                        </Select>
+                    </div>
+                    <div className="col-lg-6">
+                            <label htmlFor="">Month</label>
+                            <DatePicker  
+                            allowClear
+                            format="DD-MM-YYYY" 
+                            style={{width:'100%'}} 
+                            onChange={(date,dateString) => {
+                                let finalDate = moment(date).format('YYYY-MM-DD');
+                            }} />
+                    </div>
+                </div>
+                <div className="row mt-3 justify-content-center">
+                    {
+                        countArray && countArray.map((item,index) => (
+                            <div className="col-lg-6 mt-3" key={index}>
+                                <div className="box-card color-yellow">
+                                    <h5 className="box-card-title">{item.count}</h5>
+                                    <h5 className="box-card-text">{item.name}</h5>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HomeCard
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import React from 'react';
-import { Input,Select, DatePicker,Tabs } from 'antd';
-import moment from 'moment';
+import HomeCard from "../components/HomeCard"
 
 
 export default function Home() {
@@ -15,191 +14,42 @@ export default function Home() {
       <div className="count-card">
         <div className="row">
           <div className="col-lg-6">
-              <div className="card mt-3">
-                <div className="card-body">
-                    <h2 className="title">Billing Data</h2>
-                    <h3 className="sub-title my-4">Bill Data</h3>
-                    <div className="row">
-                      <div className="col-lg-6">
-                          <label htmlFor="">Circle</label>
-                          <Select
-                              allowClear
-                              showSearch
-                              // value={objArr && objArr[item.value]}
-                              style={{ width: '100%' }}
-                              // onChange={(val) => onChangeHandler(val,item.value)}
-                              >
-                              <Select.Option> 
-                                {"val"}
-                              </Select.Option>
-                          </Select>
-                       </div>
-                       <div className="col-lg-6">
-                        <label htmlFor="">Division</label>
-                          <Select
-                              allowClear
-                              showSearch
-                              // value={objArr && objArr[item.value]}
-                              style={{ width: '100%' }}
-                              // onChange={(val) => onChangeHandler(val,item.value)}
-                              >
-                              <Select.Option> 
-                                {"val"}
-                              </Select.Option>
-                          </Select>
-                       </div>
-                       <div className="col-lg-6">
-                        <label htmlFor="">Zone</label>
-                          <Select
-                              allowClear
-                              showSearch
-                              // value={objArr && objArr[item.value]}
-                              style={{ width: '100%' }}
-                              // onChange={(val) => onChangeHandler(val,item.value)}
-                              >
-                              <Select.Option> 
-                                {"val"}
-                              </Select.Option>
-                          </Select>
-                       </div>
-                       <div className="col-lg-6">
-                            <label htmlFor="">Month</label>
-                            <DatePicker  
-                              allowClear
-                              format="DD-MM-YYYY" 
-                              style={{width:'100%'}} 
-                              onChange={(date,dateString) => {
-                                  let finalDate = moment(date).format('YYYY-MM-DD');;
-                                  // return onChangeHandler(finalDate,item.value)
-                              }} />
-                       </div>
-                    </div>
-                    <div className="row mt-3">
-                      <div className="col-lg-6 mt-3">
-                        <div className="box-card color-yellow">
-                            <h5 className="box-card-title">30332</h5>
-                            <h5 className="box-card-text">Total Consumer Billed</h5>
-                        </div>
-                      </div>
-                      <div className="col-lg-6 mt-3">
-                        <div className="box-card color-grey">
-                            <h5 className="box-card-title">30332</h5>
-                            <h5 className="box-card-text">Current Bill Amount</h5>
-                        </div>
-                      </div>
-                      <div className="col-lg-6 mt-3">
-                        <div className="box-card color-grey">
-                            <h5 className="box-card-title">30332</h5>
-                            <h5 className="box-card-text">Arrears</h5>
-                        </div>
-                      </div>
-                      <div className="col-lg-6 mt-3">
-                        <div className="box-card color-grey">
-                            <h5 className="box-card-title">30332</h5>
-                            <h5 className="box-card-text">Total Bill Amount</h5>
-                        </div>
-                      </div>
-                    </div>
-                </div>
-            </div>
+            <HomeCard title="Billing Data" 
+              countArray={[
+                {name:"Total Consumer Billed",count:30332},
+                {name:"Current Bill Amount",count:30332},
+                {name:"Arrears",count:30332},
+                {name:"Total Bill Amount",count:30332},
+              ]}
+            />
           </div>
           <div className="col-lg-6">
-              <div className="card mt-3">
-                <div className="card-body">
-                  <h2 className="title">Payments Data</h2>
-                  <Tabs defaultActiveKey="1">
-                    <Tabs.TabPane tab="Against Current Bill " key="1">
-                      <div className="row">
-                        <div className="col-lg-6">
-                            <label htmlFor="">Circle</label>
-                            <Select
-                                allowClear
-                                showSearch
-                                // value={objArr && objArr[item.value]}
-                                style={{ width: '100%' }}
-                                // onChange={(val) => onChangeHandler(val,item.value)}
-                                >
-                                <Select.Option> 
-                                  {"val"}
-                                </Select.Option>
-                            </Select>
-                        </div>
-                        <div className="col-lg-6">
-                          <label htmlFor="">Division</label>
-                            <Select
-                                allowClear
-                                showSearch
-                                // value={objArr && objArr[item.value]}
-                                style={{ width: '100%' }}
-                                // onChange={(val) => onChangeHandler(val,item.value)}
-                                >
-                                <Select.Option> 
-                                  {"val"}
-                                </Select.Option>
-                            </Select>
-                        </div>
-                        <div className="col-lg-6">
-                          <label htmlFor="">Zone</label>
-                            <Select
-                                allowClear
-                                showSearch
-                                // value={objArr && objArr[item.value]}
-                                style={{ width: '100%' }}
-                                // onChange={(val) => onChangeHandler(val,item.value)}
-                                >
-                                <Select.Option> 
-                                  {"val"}
-                                </Select.Option>
-                            </Select>
-                        </div>
-                        <div className="col-lg-6">
-                              <label htmlFor="">Month</label>
-                              <DatePicker  
-                                allowClear
-                                format="DD-MM-YYYY" 
-                                style={{width:'100%'}} 
-                                onChange={(date,dateString) => {
-                                    let finalDate = moment(date).format('YYYY-MM-DD');;
-                                    // return onChangeHandler(finalDate,item.value)
-                                }} />
-                        </div>
-                      </div>
-                      <div className="row mt-3">
-                        <div className="col-lg-6 mt-3">
-                          <div className="box-card color-yellow">
-                              <h5 className="box-card-title">30332</h5>
-                              <h5 className="box-card-text">Total Consumer Billed</h5>
-                          </div>
-                        </div>
-                        <div className="col-lg-6 mt-3">
-                          <div className="box-card color-grey">
-                              <h5 className="box-card-title">30332</h5>
-                              <h5 className="box-card-text">Current Bill Amount</h5>
-                          </div>
-                        </div>
-                        <div className="col-lg-6 mt-3">
-                          <div className="box-card color-grey">
-                              <h5 className="box-card-title">30332</h5>
-                              <h5 className="box-card-text">Arrears</h5>
-                          </div>
-                        </div>
-                        <div className="col-lg-6 mt-3">
-                          <div className="box-card color-grey">
-                              <h5 className="box-card-title">30332</h5>
-                              <h5 className="box-card-text">Total Bill Amount</h5>
-                          </div>
-                        </div>
-                      </div>
-                    </Tabs.TabPane>
-                    <Tabs.TabPane tab="Against Arrears" key="2">
-                      Data 2
-                    </Tabs.TabPane>
-                    <Tabs.TabPane tab="Against Bill Net" key="3">
-                      Data 3
-                    </Tabs.TabPane>
-                  </Tabs>
-                </div>
-              </div>
+            <HomeCard title="Payments Data" 
+              countArray={[
+                {name:"Total Consumer Billed",count:30332},
+                {name:"Current Bill Amount",count:30332},
+                {name:"Arrears",count:30332},
+                {name:"Total Bill Amount",count:30332},
+              ]}
+            />
+          </div>
+          <div className="col-lg-6">
+            <HomeCard title="Billing Complaints Data" 
+              countArray={[
+                {name:"Received Count",count:30332},
+                {name:"Closed Count",count:30332},
+                {name:"Pending Count",count:30332},
+              ]}
+            />
+          </div>
+          <div className="col-lg-6">
+            <HomeCard title="FOC Complaints Data" 
+              countArray={[
+                {name:"Received Count",count:30332},
+                {name:"Closed Count",count:30332},
+                {name:"Pending Count",count:30332},
+              ]}
+            />
           </div>
         </div>
       </div>
